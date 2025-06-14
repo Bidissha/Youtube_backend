81554493f39a85d1019a4dfadfdd719c1363796e
Bidisha <bidishagarai2003@gmail.com>
2025-06-14 12:23:00 +0530

    adding utilities to the project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import 'dotenv/config'
 import connectDB from "./db/index.js"
 
 
-connectDB();
-
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running at port ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("mongoDB connection failure: ", err);
+})
 
 
 
