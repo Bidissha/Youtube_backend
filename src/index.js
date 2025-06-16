358647a6f7619fd4dff927050bdbdad69a616a9b
Bidisha <bidishagarai2003@gmail.com>
2025-06-16 23:44:28 +0530

    made user and video model, and dealt with file 2 step file uploading process using multer and cloudinary

--- src/index.js
+++ src/index.js
@@ -1,39 +1,17 @@
 //require('dotenv').config({path:'./env'});
-import 'dotenv/config'
-import connectDB from "./db/index.js"
-
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
-.then(()=>{
-  app.listen(process.env.PORT || 8000, ()=>{
-    console.log(`Server is running at port ${process.env.PORT}`);
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
   })
-})
-.catch((err)=>{
-  console.log("mongoDB connection failure: ", err);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .catch((err) => {
+    console.log("mongoDB connection failure: ", err);
+  });
 
 /*
 
---
+++ src/models/user.model.js
@@ -0,0 +1,90 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userShema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, //to make it searchable
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url(using 3rd party service to upload image and use the generated url)
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: {
+      type: Schema.Types.ObjectId,
+      ref: "video",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+    refreshTable: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//password encryption
+userShema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+//check for correct password
+userShema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//jwt code
+userShema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userShema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_REFRESH_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userShema);
